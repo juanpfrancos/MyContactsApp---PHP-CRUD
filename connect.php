4ead7082e9ab33425e40692e1b7ab897c26a6e35
Juan Pablo Franco Sanchez <juanpanime@gmail.com>
2022-02-09 13:00:47 -0500

    Update server credential for db connection.

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
-	$dbHost = 'localhost';
-	$dbName = 'db01';
+	$dbHost = '143.244.179.65';
+	$dbName = 'contacts_db';
 	$dbUser = 'root';
-	$dbPassw = '';
+	$dbPassw = 'Pass.123@5-.5';
 	$mysqli = mysqli_connect($dbHost, $dbUser, $dbPassw, $dbName); 
 ?>
