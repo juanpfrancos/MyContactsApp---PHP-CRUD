ba5296b5675fd2b518bbe26426b47c91459be225
Juan Pablo Franco Sanchez <juanpanime@gmail.com>
2022-02-09 12:47:36 -0500

    Add database structure.

---
+++ contact.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE  IF NOT EXISTS `contacts_db`;
+USE `contacts_db`;
+CREATE TABLE `contact` (
+  `id` int NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `age` int NOT NULL,
+  `phone` int NOT NULL,
+  `email` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+)
